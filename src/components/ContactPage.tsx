9aef987a8e8d9438d43efd74a6efef54c25cdf5c
Zylophixx <zylophixx@gmail.com>
2025-07-26 23:47:10 -0700

    Updated ContactPage.tsx

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -42,10 +42,7 @@ const ContactPage: React.FC<ContactPageProps> = ({ onBack }) => {
         <div className="absolute top-[25%] right-[15%] text-purple-400/20 text-5xl transform -rotate-6">
           🎬
         </div>
-        <div className="absolute top-[15%] left-[45%] text-blue-300/20 text-4xl transform rotate-45">
-          ✂️
-        </div>
-        <div className="absolute top-[60%] left-[20%] text-purple-300/20 text-5xl transform -rotate-12">
+        <div className="absolute top-[15%] left-[40%] text-purple-300/20 text-5xl transform -rotate-12">
           🎞️
         </div>
         <div className="absolute top-[70%] right-[25%] text-blue-400/20 text-6xl transform rotate-6">
